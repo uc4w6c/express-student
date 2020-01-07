7cb42c27c1221924d1d48d8a2cea169b8dd41bb2
Yuta <uc4w6c@bma.biglobe.ne.jp>
2020-01-08 00:15:31 +0900

    memo追加

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # express-student
+
+application.jsの
+methods.forEach(function(method){
+でnode.jsで利用可能なhttp methodの一覧を取得&処理
--- http.js
+++ http.js
@@ -7,4 +7,3 @@ http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello World\n');
   }).listen(3000, '127.0.0.1');
-  
