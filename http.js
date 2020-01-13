af08c162784ceeb047b685f934c90836783993c6
Yuta <uc4w6c@bma.biglobe.ne.jp>
2020-01-13 17:54:55 +0900

    Expressをまとめ

--- http.js
+++ http.js
@@ -3,6 +3,19 @@ const http = require('http')
 var app = function(req, res, next) {
   app.handle(req, res, next);
 };
+
+const Hello = {
+  say: function() {
+    console.log('Hi!' + this.name);
+  }
+};
+var hello = Object.create(Hello);
+
+app.hello = Object.create(hello, {
+  name: { value: 'Taro' }
+})
+app.hello.say();
+
 app.handle = function handle(req, res, callback) {
   console.log('hello');
   res.writeHead(200, {'Content-Type': 'text/plain'});
